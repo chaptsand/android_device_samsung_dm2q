cdf93c819362c5d7dd62dd9ac9187cd55371eb4f
chaptsand <chaptsand@gmail.com>
2025-03-01 01:41:05 +0800

    dm2q: Yet another AOSP project ify
    Change-Id: I273b82c0d684146f9b36a4c4b059c6914c7d57cf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dm2q.mk
+    $(LOCAL_DIR)/yaap_dm2q.mk
--- yaap_dm2q.mk
+++ yaap_dm2q.mk
@@ -11,19 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/samsung/dm2q/device.mk)
 
-# Inherit from the Lineage configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the YAAP configuration.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_dm2q
+PRODUCT_NAME := yaap_dm2q
 PRODUCT_DEVICE := dm2q
-PRODUCT_BRAND := Samsung
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S9160
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_CHARACTERISTICS := nosdcard
+YAAP_BUILDTYPE := Voyager
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm2qzhx-user 14 UP1A.231005.007 S9160ZHS5CYA1 release-keys" \
-    BuildFingerprint=samsung/dm2qzhx/dm2q:14/UP1A.231005.007/S9160ZHS5CYA1:user/release-keys \
+    BuildDesc="dm2qzhx-user 14 UP1A.231005.007 S9160ZHS6CYC1 release-keys" \
+    BuildFingerprint=samsung/dm2qzhx/dm2q:14/UP1A.231005.007/S9160ZHS6CYC1:user/release-keys \
     DeviceProduct=dm2qzhx \
     SystemName=dm2qzhx
